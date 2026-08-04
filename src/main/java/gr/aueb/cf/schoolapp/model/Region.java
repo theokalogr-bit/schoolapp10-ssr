c126e292c26338cd15d3adfe0ef982b52a3483a0
theokalogr <theokalogr@gmail.com>
2026-08-05 01:02:25 +0300

    Add Region entity with relationships to Teacher

---
+++ src/main/java/gr/aueb/cf/schoolapp/model/Region.java
@@ -0,0 +1,45 @@
+package gr.aueb.cf.schoolapp.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "regions")
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Getter(AccessLevel.PACKAGE)
+    @Setter(AccessLevel.NONE)
+    @OneToMany(mappedBy = "region", fetch = FetchType.LAZY)
+    private Set<Teacher> teachers = new HashSet<>();
+
+    public Set<Teacher> getAllTeachers() {
+        return Collections.unmodifiableSet(teachers);
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+        teacher.setRegion(this);
+    }
+
+    public void removeTeacher(Teacher teacher) {
+        teachers.remove(teacher);
+        teacher.setRegion(null);
+    }
+}
